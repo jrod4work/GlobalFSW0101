53df3112b5493b131e7527a1abc8f27515ab2015
Unknown <jrod4work@hotmail.com>
2018-06-22 13:52:10 -0600

    a

---
+++ DOM/1chapter/js.js
@@ -0,0 +1,24 @@
+
+//var myButton = document.getElementById("submitButton");
+
+var firstName = document.getElementById('firstName').value;
+var lastName = document.getElementById('lastName').value;
+var email = document.getElementById('email').value;
+
+
+var fruit = document.getElementsByTagName("li");
+
+
+var text = document.getElementsByClassName("paragraph");
+
+var text = document.getElementById("paragraph").innerHTML;
+
+var text = document.getElementById("paragraph").innerHTML;
+
+document.getElementById("paragraph").innerHTML = text + " and I am awesome!";
+
+var myButton = document.getElementById("submitButton");
+
+myButton.addEventListener("click", function(){
+  alert("Click!!");
+})
---
+++ DOM/FEFL04Practice/js.js
@@ -0,0 +1,3 @@
+var parentNode = document.getElementById("div1");
+var childNode = document.getElementById("p1");
+parentNode.removeChild(childNode);
