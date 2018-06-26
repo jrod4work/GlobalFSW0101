84442d72daa6a92a0056e16d4e9471ac09e1bf14
Unknown <jrod4work@hotmail.com>
2018-06-25 20:41:50 -0600

    Final Project

---
+++ Ajax/1-httpRequestExample/script.js
@@ -0,0 +1,29 @@
+/* 1st example
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("example").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "info.txt", true);
+  xhttp.send();
+} */
+
+//second example
+
+/* function loadDoc(url, callback) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function myFunction(xhttp) {
+  document.getElementById("example").innerHTML = xhttp.responseText;
+} */
--- Ajax/3-jsonExample/script.js
+++ Ajax/3-jsonExample/script.js
@@ -3,6 +3,8 @@ newRequest.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     let myObj = JSON.parse(this.responseText);
     document.getElementById("example").innerHTML = myObj.name;
+    document.getElementById("example2").innerHTML = myObj.age;
+    //document.getElementById("example3").innerHTML = myObj.cars;
   }
 };
 newRequest.open("GET", "new.json", true);
